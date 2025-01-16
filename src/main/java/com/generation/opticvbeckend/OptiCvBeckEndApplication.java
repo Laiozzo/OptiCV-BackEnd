0b184cf7677dfd2f3fd34798813c0a7d2ec47282
nicola <nicolaaprile123@gmail.com>
2025-01-16 15:35:02 +0100

    0.0

---
+++ src/main/java/com/generation/opticvbeckend/OptiCvBeckEndApplication.java
@@ -0,0 +1,13 @@
+package com.generation.opticvbeckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiCvBeckEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptiCvBeckEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/generation/opticvbeckend/OptiCvBeckEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.opticvbeckend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiCvBeckEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

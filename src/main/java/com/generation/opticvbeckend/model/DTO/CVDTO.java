eb9e02de2c33392a97c112cb6a043d9789a61522
nicola <nicolaaprile123@gmail.com>
2025-01-18 23:52:50 +0100

    0.2d ParseCV (da ultimare) UploadedFile AI - Tutto da rifare mi sa. Teniamo questa come base e vediamo come si evolve

--- src/main/java/com/generation/opticvbeckend/model/DTO/CVDTO.java
+++ src/main/java/com/generation/opticvbeckend/model/DTO/CVDTO.java
@@ -1,60 +1,48 @@
-package com.generation.opticvbeckend.model.entities;
+package com.generation.opticvbeckend.model.DTO;
 
-import jakarta.persistence.*;
-
-@Entity
-public class CV extends BaseEntity
-{
-    //Relationship
-    @ManyToOne
-    @JoinColumn(name="user_id")
-    private User user;
-
+public class CVDTO {
     private String name;
+    private String gender;
     private String contactDetails;
     private String education;
     private String workExperience;
     private String skills;
 
-    public CV(String name,  String contactDetails, String education, String workExperience, String skills) {
+    //Costruttore
+    public CVDTO(String name, String gender, String contactDetails, String education, String workExperience, String skills) {
         this.name = name;
+        this.gender = gender;
         this.contactDetails = contactDetails;
         this.education = education;
         this.workExperience = workExperience;
         this.skills = skills;
     }
 
-    public CV() {
-
-    }
-
-
-    //Getter and Setter
-
+    //Getter and setter
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
     public String getContactDetails() {
         return contactDetails;
     }
-
     public void setContactDetails(String contactDetails) {
         this.contactDetails = contactDetails;
     }
-
     public String getEducation() {
         return education;
     }
-
     public void setEducation(String education) {
         this.education = education;
     }
-
     public String getWorkExperience() {
         return workExperience;
     }
@@ -62,24 +50,22 @@ public class CV extends BaseEntity
     public void setWorkExperience(String workExperience) {
         this.workExperience = workExperience;
     }
-
     public String getSkills() {
         return skills;
     }
-
     public void setSkills(String skills) {
         this.skills = skills;
     }
 
     @Override
     public String toString() {
-        return "CV {" +
-                "\n Name: " + name +
-                "\n Contact Details: " + contactDetails +
-                "\n Education: " + education +
-                "\n Work Experience: " + workExperience +
-                "\n Skills: " + skills +
-                "\n}";
+        return "CVDTO{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", contactDetails='" + contactDetails + '\'' +
+                ", education='" + education + '\'' +
+                ", workExperience='" + workExperience + '\'' +
+                ", skills='" + skills + '\'' +
+                '}';
     }
-
 }

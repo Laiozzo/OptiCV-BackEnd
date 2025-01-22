eaabcdb04ac0e833f50b91cba21f1e2b4fb5eb93
nicola <nicolaaprile123@gmail.com>
2025-01-22 14:36:56 +0100

    Add CORS configuration and comment out unused CVMapper.
    Introduced a global CORS configuration to allow requests from the frontend. The `CVMapper` class was commented out as it is currently not in use, preserving it for potential future reference or reactivation. Additionally, a `@CrossOrigin` annotation was added to `JobTypeController` for finer control of cross-origin requests.

--- src/main/java/com/generation/opticvbeckend/controllers/JobTypeController.java
+++ src/main/java/com/generation/opticvbeckend/controllers/JobTypeController.java
@@ -4,6 +4,7 @@ import com.generation.opticvbeckend.controllers.helpers.ControllerHelper;
 import com.generation.opticvbeckend.model.dto.JobTypeDTO;
 import com.generation.opticvbeckend.model.dto.JobTypeListDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/generation/opticvbeckend/mapper/CVMapper.java
+++ src/main/java/com/generation/opticvbeckend/mapper/CVMapper.java
@@ -1,16 +1,16 @@
-package com.generation.opticvbeckend.mapper;
-
-import com.generation.opticvbeckend.model.DTO.CVDTO;
-import com.generation.opticvbeckend.model.entities.CV;
-
-public class CVMapper {
-    public static CV fromDTO(CVDTO dto){
-        return new CV(
-                dto.getName(),
-                dto.getContactDetails(),
-                dto.getEducation(),
-                dto.getWorkExperience(),
-                dto.getSkills()
-        );
-    }
-}
+//package com.generation.opticvbeckend.mapper;
+//
+//import com.generation.opticvbeckend.model.DTO.CVDTO;
+//import com.generation.opticvbeckend.model.entities.CV;
+//
+//public class CVMapper {
+//    public static CV fromDTO(CVDTO dto){
+//        return new CV(
+//                dto.getName(),
+//                dto.getContactDetails(),
+//                dto.getEducation(),
+//                dto.getWorkExperience(),
+//                dto.getSkills()
+//        );
+//    }
+//}
